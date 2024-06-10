d05914533362bea1d4d71fdc361fb9f7e2e96fea
Shana <Shannaael@gmail.com>
2024-06-10 14:28:55 +0530

    update categories

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -133,9 +133,11 @@ export default function UpdatePost() {
             value={formData.category}
           >
             <option value="uncategorized">Select a category</option>
-            <option value="javascript">JavaScript</option>
-            <option value="reactjs">React.js</option>
-            <option value="nextjs">Next.js</option>
+            <option value="Beginner">Beginners</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Advanced">Advanced</option>
+            <option value="Tips&Tricks">Tips & Tricks</option>
+            <option value="KnittingPatterns">Knitting Patterns</option>
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
